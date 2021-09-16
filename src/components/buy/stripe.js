fed243ab71ba5f613c5a04b0e5858e41befebf4b
WEB DEV <79596679+muhammad3132002@users.noreply.github.com>
2021-09-16 21:21:18 +1000

    Update stripe.js

--- src/components/buy/stripe.js
+++ src/components/buy/stripe.js
@@ -22,24 +22,7 @@ export default function Stripe(props) {
     const [clientSecret, setClientSecret] = useState('');
     const stripe = useStripe();
     const elements = useElements();
-  
-    useEffect(() => {
-      // Create PaymentIntent as soon as the page loads
-      window
-        .fetch("/create-payment-intent", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({items: [{ id: "xl-tshirt" }]})
-        })
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          setClientSecret(data.clientSecret);
-        });
-    }, []);
+ 
   
     const cardStyle = {
       style: {
